439814af502692c4bc9f70ed9e4ec44fb38c7754
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-03-24 13:45:09 +0800

    add Header, Navigation, and Logo components; implement RootLayout with global styles

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+console.log(josefin);
+
+import "@/app/_styles/globals.css";
+import Header from "@/app/_components/Header";
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+export const metadata = {
+  title: {
+    template: "%s - The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of Italian Dolomites, surrounded by beautiful mountains and dark forests",
+};
+
+function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body
+        className={`bg-primary-950 text-primary-100 min-h-screen flex antialiased flex-col relative ${josefin.className}`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto  ">{children}</main>
+        </div>
+        <footer>Copyright by The Wild Oasis</footer>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
