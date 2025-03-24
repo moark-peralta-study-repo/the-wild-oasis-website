3e61e8df8c2d52dc443523e8e92c488a197d39dd
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-03-24 16:14:52 +0800

    refactor layout component to improve structure and styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,6 @@ const josefin = Josefin_Sans({
   display: "swap",
 });
 
-console.log(josefin);
-
 import "@/app/_styles/globals.css";
 import Header from "@/app/_components/Header";
 
@@ -30,8 +28,8 @@ function RootLayout({ children }: RootLayoutProps) {
         className={`bg-primary-950 text-primary-100 min-h-screen flex antialiased flex-col relative ${josefin.className}`}
       >
         <Header />
-        <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl mx-auto  ">{children}</main>
+        <div className="flex-1 px-8 py-12 grid ">
+          <main className="max-w-7xl mx-auto  w-full">{children}</main>
         </div>
         <footer>Copyright by The Wild Oasis</footer>
       </body>
