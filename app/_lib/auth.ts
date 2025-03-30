7f5dd81ff2400dd4582a70027c4e2a49fd337a68
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-03-30 20:17:38 +0800

    enhance authentication flow with guest user handling

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,11 +1,23 @@
-import NextAuth, { Session } from "next-auth";
+import NextAuth, { Session, User } from "next-auth";
 import Google from "next-auth/providers/google";
 import { NextRequest } from "next/server";
+import { createGuest, getGuest } from "@/app/_lib/data-service";
 
 type AuthType = {
   auth: Session | null;
   request: NextRequest;
 };
+
+interface CustomSession extends Session {
+  user:
+    | {
+        name?: string;
+        email?: string;
+        image?: string;
+        id?: string;
+      }
+    | undefined;
+}
 
 const authConfig = {
   providers: [
@@ -15,8 +27,37 @@ const authConfig = {
     }),
   ],
   callbacks: {
-    authorized({ auth, request }: AuthType) {
+    authorized({ auth }: AuthType) {
       return !!auth?.user;
+    },
+    async signIn({ user }: { user: User }) {
+      try {
+        if (!user.email) return false;
+        const existingGuest = await getGuest(user.email);
+        if (!existingGuest) {
+          await createGuest({
+            email: user.email,
+            fullName: user.name || "",
+          });
+        }
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session }: { session: Session }) {
+      const customSession = session as CustomSession;
+
+      if (customSession.user?.email) {
+        const guest = await getGuest(customSession.user.email);
+
+        if (guest) {
+          customSession.user.id = String(guest.id);
+        }
+      }
+
+      return customSession;
     },
   },
   pages: {
