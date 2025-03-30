f83f4810a91cb34c02b67af99398962e877f3b7b
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-03-30 17:41:44 +0800

    update Page component to greet users by their first name from session

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,11 +1,19 @@
+import { auth } from "@/app/_lib/auth";
+
 export const metadata = {
   title: "Guest Area",
 };
 
-function Page() {
+async function Page() {
+  const session = await auth();
+
+  const firstName = session?.user?.name?.split(" ")[0] ?? "Guest";
+  const capitalizedFirstName =
+    firstName.charAt(0).toUpperCase() + firstName.slice(1);
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Moark Peralta
+      Welcome, {capitalizedFirstName}!
     </h2>
   );
 }
