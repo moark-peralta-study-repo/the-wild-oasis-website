58fafe502a5ed1578afccad3d3d789079273d1dd
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-03-30 16:18:21 +0800

    add user authentication display in ReservationForm component

--- app/_components/ReservationForm.tsx
+++ app/_components/ReservationForm.tsx
@@ -1,15 +1,13 @@
 // "use client";
 
-import { CabinsAPIResponse } from "@/app/types/types";
-import { useReservation } from "@/app/contexts/ReservationContext";
+import { CabinsAPIResponse, UserAuthResponse } from "@/app/types/types";
 
 type ReservationProps = {
   cabin: CabinsAPIResponse;
+  user: UserAuthResponse;
 };
 
-function ReservationForm({ cabin }: ReservationProps) {
-  // const { range } = useReservation();
-
+function ReservationForm({ cabin, user }: ReservationProps) {
   const { maxCapacity } = cabin;
 
   return (
@@ -17,16 +15,16 @@ function ReservationForm({ cabin }: ReservationProps) {
       <div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center">
         <p>Logged in as</p>
 
-        {/* <div className='flex gap-4 items-center'>
+        <div className="flex gap-4 items-center">
           <img
             // Important to display google profile images
-            referrerPolicy='no-referrer'
-            className='h-8 rounded-full'
+            referrerPolicy="no-referrer"
+            className="h-8 rounded-full"
             src={user.image}
             alt={user.name}
           />
           <p>{user.name}</p>
-        </div> */}
+        </div>
       </div>
 
       <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col">
@@ -61,7 +59,7 @@ function ReservationForm({ cabin }: ReservationProps) {
           />
         </div>
 
-        <div className="flex justify-end items-center gap-6">
+        <div className="flex austify-end items-center gap-6">
           <p className="text-primary-300 text-base">Start by selecting dates</p>
 
           <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
