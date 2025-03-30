5f72d45f58b8b7c94d0667f3df1eabbe95696561
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-03-30 17:47:33 +0800

    add signOutAction to handle user sign-out functionality

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -1,9 +1,15 @@
 "use server";
 
-import { signIn } from "@/app/_lib/auth";
+import { signIn, signOut } from "@/app/_lib/auth";
 
 export async function signInAction() {
   await signIn("google", {
     redirectTo: "/account",
+  });
+}
+
+export async function signOutAction() {
+  await signOut({
+    redirectTo: "/",
   });
 }
