715939207dba6d27ca81092dd8c3684ff73056c3
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-03-30 09:41:55 +0800

    add ReservationContext and provider for managing reservation state

---
+++ app/contexts/ReservationContext.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+import { DateRange } from "react-day-picker";
+
+type ReservationContextType = {
+  range: DateRange | undefined;
+  setRange: (dateRange: DateRange | undefined) => void;
+};
+type ReservationContextProp = {
+  children: React.ReactNode;
+};
+
+const ReservationContext = createContext<ReservationContextType | null>(null);
+
+const initialState = { from: undefined, to: undefined } as DateRange;
+
+function ReservationProvider({ children }: ReservationContextProp) {
+  const [range, setRangeState] = useState<DateRange | undefined>(initialState);
+  function setRange(dateRange: DateRange | undefined) {
+    setRangeState(dateRange ?? initialState);
+  }
+  return (
+    <ReservationContext.Provider value={{ range, setRange }}>
+      {children}
+    </ReservationContext.Provider>
+  );
+}
+
+function useReservation() {
+  const context = useContext(ReservationContext);
+  if (!context) {
+    throw new Error("useReservation must be used within the context");
+  }
+
+  return context;
+}
+
+export { ReservationProvider, useReservation };
