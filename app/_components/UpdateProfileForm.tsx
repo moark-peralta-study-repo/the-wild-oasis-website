3cd60cc6b0e0d249e5e52a153feb156c3062b34f
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-03-31 23:56:31 +0800

    refactor UpdateProfileForm to use SubmitButton component for profile update

--- app/_components/UpdateProfileForm.tsx
+++ app/_components/UpdateProfileForm.tsx
@@ -2,7 +2,7 @@
 
 import { GuestsAPIResponse } from "@/app/types/types";
 import { updateGuestAction } from "@/app/_lib/actions";
-import { useFormStatus } from "react-dom";
+import SubmitButton from "@/app/_components/SubmitButton";
 
 type UpdateProfileFormProps = {
   children: React.ReactNode;
@@ -60,23 +60,10 @@ function UpdateProfileForm({ children, guest }: UpdateProfileFormProps) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <Button />
+        <SubmitButton pendingLabel="Updating...">Update Profile</SubmitButton>
       </div>
     </form>
   );
 }
 
 export default UpdateProfileForm;
-
-function Button() {
-  const { pending } = useFormStatus();
-
-  return (
-    <button
-      disabled={pending}
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-    >
-      {pending ? "Updating..." : "Update profile"}
-    </button>
-  );
-}
