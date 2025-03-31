1c28035a30c9babea40e850a30b1056c0d1f6849
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-03-31 14:11:13 +0800

    add updateGuestAction for updating guest information

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -1,6 +1,8 @@
 "use server";
 
-import { signIn, signOut } from "@/app/_lib/auth";
+import { auth, signIn, signOut } from "@/app/_lib/auth";
+import { supabase } from "@/app/_lib/supabase";
+import { UpdateGuestInput } from "@/app/types/types";
 
 export async function signInAction() {
   await signIn("google", {
@@ -12,4 +14,35 @@ export async function signOutAction() {
   await signOut({
     redirectTo: "/",
   });
+}
+
+export async function updateGuestAction(formData: FormData) {
+  const session = await auth();
+  if (!session) {
+    throw new Error("You must be logged in");
+  }
+
+  const nationalIdValue = formData.get("nationalId");
+  if (!nationalIdValue) throw new Error("National ID is required");
+  const nationalId = nationalIdValue.toString();
+
+  const nationalityValue = formData.get("nationality") as string;
+
+  if (!nationalityValue) throw new Error("Nationality is required");
+  const [nationality, countryFlag] = nationalityValue.split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalId))
+    throw new Error("Please enter a valid National ID");
+
+  const updateData: UpdateGuestInput = { nationality, countryFlag, nationalId };
+
+  const { error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user?.id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Guest could not be updated");
+  }
 }
