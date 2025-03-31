6066fccedc2237f140d670012bb8ea9a93c028f7
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-03-31 18:43:18 +0800

    fix import path for DeleteReservation component in ReservationCard

--- app/_components/ReservationCard.tsx
+++ app/_components/ReservationCard.tsx
@@ -1,6 +1,6 @@
 import { PencilSquareIcon } from "@heroicons/react/24/solid";
 import { format, formatDistance, isPast, isToday, parseISO } from "date-fns";
-import DeleteReservation from "../../starter/components/DeleteReservation";
+import DeleteReservation from "./DeleteReservation";
 import Image from "next/image";
 import { BookingsAPIResponse } from "@/app/types/types";
 import Link from "next/link";
