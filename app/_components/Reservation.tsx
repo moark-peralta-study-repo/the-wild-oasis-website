832881f808332eda4ebfff91752b77d6e59c6ee8
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-04-01 18:57:36 +0800

    fix bug of not using the displayRange for the numNights variable

--- app/_components/Reservation.tsx
+++ app/_components/Reservation.tsx
@@ -1,21 +1,33 @@
 import DateSelector from "@/app/_components/DateSelector";
 import ReservationForm from "@/app/_components/ReservationForm";
 import { getBookedDatesByCabinId, getSettings } from "@/app/_lib/data-service";
-import { CabinsAPIResponse } from "@/app/types/types";
+import {
+  CabinsAPIResponse,
+  SettingsAPIResponse,
+  UserAuthResponse,
+} from "@/app/types/types";
 import { auth } from "@/app/_lib/auth";
 import LoginMessage from "@/app/_components/LoginMessage";
+import { Session } from "next-auth";
 
 type ReservationProps = {
   cabin: CabinsAPIResponse;
 };
 
 async function Reservation({ cabin }: ReservationProps) {
-  const [settings, bookedDates] = await Promise.all([
-    getSettings(),
-    getBookedDatesByCabinId(cabin.id),
-  ]);
+  const [settings, bookedDates]: [SettingsAPIResponse, Date[]] =
+    await Promise.all([getSettings(), getBookedDatesByCabinId(cabin.id)]);
 
-  const session = await auth();
+  const session: Session | null = await auth();
+
+  const userAuth: UserAuthResponse | null = session?.user
+    ? ({
+        id: session.user.id || "",
+        name: session.user.name || "",
+        email: session.user.email || "",
+        image: session.user.image || "",
+      } as UserAuthResponse)
+    : null;
 
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
@@ -24,8 +36,8 @@ async function Reservation({ cabin }: ReservationProps) {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      {session?.user ? (
-        <ReservationForm cabin={cabin} user={session.user} />
+      {userAuth ? (
+        <ReservationForm cabin={cabin} user={userAuth} />
       ) : (
         <LoginMessage />
       )}
